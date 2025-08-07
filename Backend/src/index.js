7d9346687a242a06c4f3cc5a603321257e2df5c7
rai3210 <skraixt@gmail.com>
2025-08-07 21:34:20 +0530

    backend setup id done

---
+++ Backend/src/index.js
@@ -0,0 +1,44 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app}from './app.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT ||  9000, ()=>{
+        console.log(`Server is running on port: ${process.env.PORT}`);
+        
+    })
+})
+.then((error)=>{
+    console.log("Mongo db Connection failed !!!", error); 
+})
+
+/*
+
+import express from "express";
+
+const app = express()
+(async () => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI} /${DB_NAME}`)
+       app.on("error", (error)=>{
+        console.log("ERR: ",error);
+        throw error
+        
+       })
+       app.listen(process.env.PORT, ()=>{
+        console.log(`APP is listening on port ${process.env.PORT}`);
+        
+       })
+        
+    } catch (error) {
+        console.error("ERROR: ",error)
+    }
+})()
+
+*/
